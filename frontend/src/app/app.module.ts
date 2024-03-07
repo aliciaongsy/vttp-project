4243b1718494dc6b95fcef8614360832f2120748
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-08 00:16:48 +0800

    updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './component/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './component/register/register.component';
+import { TaskMasterComponent } from './component/task-master/task-master.component';
+import { AttributionsComponent } from './component/attributions/attributions.component';
+import { FeaturesComponent } from './component/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    TaskMasterComponent,
+    AttributionsComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
