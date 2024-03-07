cc2907e80fc689b6318f49b94f3f9849eefbdb76
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-08 00:17:02 +0800

    add attributions

---
+++ frontend/src/app/component/attributions/attributions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attributions',
+  templateUrl: './attributions.component.html',
+  styleUrl: './attributions.component.css'
+})
+export class AttributionsComponent {
+
+}
