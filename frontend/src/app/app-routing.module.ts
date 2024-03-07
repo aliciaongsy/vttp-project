dbd01bbed07506b955a1a2485fc3122ef3760d89
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-08 00:16:17 +0800

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { TaskMasterComponent } from './component/task-master/task-master.component';
+import { FeaturesComponent } from './component/features/features.component';
+import { AttributionsComponent } from './component/attributions/attributions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'taskmaster', component: TaskMasterComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'attributions', component: AttributionsComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
