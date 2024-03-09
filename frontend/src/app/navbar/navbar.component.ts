ee00288ba5790e47d49c87356ee5c3866d10b10b
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-09 16:47:04 +0800

    subscribe to login status

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { DbStore } from '../service/db.store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  private store = inject(DbStore)
+
+  loginStatus!: Observable<boolean>
+
+  ngOnInit(): void {
+    this.loginStatus = this.store.getStatus
+    this.loginStatus.subscribe((value) => console.info(value))
+  }
 }
