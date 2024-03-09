1a639b0604377934057921acdca0cce09e1f5d2f
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-09 16:45:25 +0800

    process login

--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DbStore } from '../../service/db.store';
+import { UserService } from '../../service/user.service';
+import { LoginDetails } from '../../model';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +12,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit{
 
   private fb = inject(FormBuilder)
+  private userSvc = inject(UserService)
+  private store = inject(DbStore)
 
   form!: FormGroup
+  invalid: boolean = false
 
   ngOnInit(): void {
     this.form = this.createForm();
@@ -24,6 +30,22 @@ export class LoginComponent implements OnInit{
   }
 
   login(){
-    // login to account
+    const details = this.form.value as LoginDetails
+    this.userSvc.checkUserExist(details.email)
+      .then(() => {
+        this.userSvc.checkLoginDetails(details.email, details.password)
+          .then(() => {
+            // valid email and password
+            this.store.changeStatus()
+          })
+          .catch(() => {
+            // valid email, invalid password
+            this.invalid = true
+          })
+      })
+      .catch(() => {
+        // invalid email
+        this.invalid = true
+      })
   }
 }
