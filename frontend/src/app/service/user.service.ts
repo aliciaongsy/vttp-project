a24d85f9088ccd1caa7634804e4bf77419ac6a1a
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-09 18:07:02 +0800

    update endpoint

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -14,8 +14,8 @@ export class UserService {
   private http = inject(HttpClient)
 
   checkUserExist(email: string){
-    // endpoint: localhost:8080/exist/${email}
-    return firstValueFrom(this.http.get<any>(`${URL}/exist/${email}`))
+    // endpoint: localhost:8080/api/user/${email}
+    return firstValueFrom(this.http.get<any>(`${URL}/api/user/${email}`))
   }
 
   checkLoginDetails(email: string, password: string){
@@ -24,12 +24,12 @@ export class UserService {
       .set('email', email)
       .set('password', password)
 
-    // endpoint: localhost:8080/login?email=&password=
-    return firstValueFrom(this.http.get<any>(`${URL}/login`, { params }))
+    // endpoint: localhost:8080/api/login?email=&password=
+    return firstValueFrom(this.http.get<any>(`${URL}/api/login`, { params }))
   }
 
   createUser(user: NewUser){
-    // endpoint: localhost:8080/register
-    return firstValueFrom(this.http.post<any>(`${URL}/register`, user))
+    // endpoint: localhost:8080/api/register
+    return firstValueFrom(this.http.post<any>(`${URL}/api/register`, user))
   }
 }
