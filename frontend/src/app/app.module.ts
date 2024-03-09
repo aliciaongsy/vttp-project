1412bc13770984ebc307d1b8643ebefbd79bce3a
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-09 11:33:21 +0800

    styling with primeng

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './component/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './component/register/register.component';
 import { TaskMasterComponent } from './component/task-master/task-master.component';
 import { AttributionsComponent } from './component/attributions/attributions.component';
 import { FeaturesComponent } from './component/features/features.component';
+
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { SplitterModule } from 'primeng/splitter';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -25,8 +31,14 @@ import { FeaturesComponent } from './component/features/features.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    DialogModule,
+    ButtonModule,
+    SplitterModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
