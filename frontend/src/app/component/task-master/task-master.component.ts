a6cb4d99f0c7ad44cb5851a15dd35174c19de4eb
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-10 16:00:32 +0800

    add tabmenu

--- frontend/src/app/component/task-master/task-master.component.ts
+++ frontend/src/app/component/task-master/task-master.component.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DbStore } from '../../service/db.store';
+import { Observable } from 'rxjs';
+import { MenuItem } from 'primeng/api';
+import { Task } from '../../model';
 
 @Component({
   selector: 'app-task-master',
   templateUrl: './task-master.component.html',
   styleUrl: './task-master.component.css'
 })
-export class TaskMasterComponent {
+export class TaskMasterComponent implements OnInit{
+
+  private activatedRoute = inject(ActivatedRoute)
+  private store = inject(DbStore)
 
   visible: boolean = false;
-  value!: string
+
+  name!: string // workspace name
   workspaces: string[] = []
+
+  pathVariable!:string
+
+  loginStatus!: Observable<boolean>
+
+  tab: string='Overview'
+
+  tasks: Task[] = []
+
+  menuItems: MenuItem[] = [
+    { label: 'Overview', command: (event: any) => {
+      this.tab = event.item.label
+      console.info(this.tab)
+    }},
+    { label: 'Projects', command: (event: any) => {
+      this.tab = event.item.label
+      console.info(this.tab)
+    }},
+    { label: 'Tasks', command: (event: any) => {
+      this.tab = event.item.label
+      console.info(this.tab)
+    }}
+  ]
+
+  activeTab = this.menuItems[0]
+
+  ngOnInit(): void {
+    this.pathVariable=this.activatedRoute.snapshot.params['w']
+    console.info(this.activatedRoute.snapshot.params['w'])
+    this.loginStatus = this.store.getStatus
+  }
 
   showDialog() {
     this.visible = true;
@@ -17,12 +57,15 @@ export class TaskMasterComponent {
 
   createWorkspace(){
     console.info("press button")
-    console.info(this.value)
-    this.workspaces.push(this.value)
+    console.info(this.name)
+    this.workspaces.push(this.name)
     // close dialog
     this.visible=false
     // clear value
-    this.value=''
+    this.name=''
   }
 
+  onTabItemClick(i: number) {
+    console.log(this.menuItems[i])
+  }
 }
