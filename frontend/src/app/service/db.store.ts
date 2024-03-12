07757e436541e3959f03d0dbe33ec7fd2a085c23
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-12 10:59:45 +0800

    store user data and list of workspaces

--- frontend/src/app/service/db.store.ts
+++ frontend/src/app/service/db.store.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@angular/core";
 import { ComponentStore } from "@ngrx/component-store";
-import { UserSlice } from "../model";
+import { UserDetails, UserSlice } from "../model";
 
 const INIT_STORE: UserSlice = {
-    login: false
+    login: false, 
+    user: {
+        id: '',
+        name: '',
+        email: ''
+    },
+    workspaces: []
 }
 
 @Injectable()
@@ -13,18 +19,38 @@ export class DbStore extends ComponentStore<UserSlice>{
         super(INIT_STORE)
     }
 
-    readonly changeStatus = this.updater<void>(
-        (slice: UserSlice) => {
+    readonly changeStatus = this.updater<UserDetails>(
+        (slice: UserSlice, currUser: UserDetails) => {
             // change current status
             const newStatus = !(slice.login)
             this.setState({
-                login: newStatus
+                login: newStatus,
+                user: currUser,
+                workspaces: slice.workspaces // get from service
             })
             return slice
         }
     )
 
+    readonly addWorkspace = this.updater<string>(
+        (slice: UserSlice, workspace: string) => {
+            return {
+                login: slice.login,
+                user: slice.user,
+                workspaces: [...slice.workspaces, workspace]
+            }
+        }
+    )
+
     readonly getStatus = this.select<boolean>(
         (slice: UserSlice) => slice.login
+    )
+
+    readonly getUser = this.select<UserDetails>(
+        (slice: UserSlice) => slice.user
+    )
+
+    readonly getWorkspaces = this.select<string[]>(
+        (slice: UserSlice) => slice.workspaces
     )
 }
