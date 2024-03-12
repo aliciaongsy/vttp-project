3d527cae0a7a0ae652d73c516b077a99a1cf998d
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-13 01:55:21 +0800

    link to backend

--- frontend/src/app/component/task-master/tasks/tasks.component.ts
+++ frontend/src/app/component/task-master/tasks/tasks.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Task } from '../../../model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TaskService } from '../../../service/task.service';
+import { ActivatedRoute } from '@angular/router';
+import { DbStore } from '../../../service/db.store';
+import { Observable, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit {
 
   private fb = inject(FormBuilder)
+  private taskSvc = inject(TaskService)
+  private activatedRoute = inject(ActivatedRoute)
+  private store = inject(DbStore)
+
+  currentWorkspace!: string
+  uid!: string
+
   taskForm!: FormGroup
 
-  tasks: Task[] = []
+  tasks!: Observable<Task[]>
   minDate!: Date
 
   visible: boolean = false
@@ -22,23 +33,39 @@ export class TasksComponent implements OnInit{
   ngOnInit(): void {
     this.taskForm = this.createForm()
     this.minDate = new Date()
+
+    // get workspace name - get parent path variable {workspace}/tasks
+    this.currentWorkspace = this.activatedRoute.parent?.snapshot.params['w']
+    // get user id
+    firstValueFrom(this.store.getUser).then((value) => {
+      this.uid = value.id
+      // retrieve task from mongodb
+      this.tasks = this.taskSvc.getTasksOfWorkspace(this.uid, this.currentWorkspace)
+      firstValueFrom(this.tasks).then((value) => console.info(value))
+    }
+    )
   }
 
-  createForm(): FormGroup{
+  createForm(): FormGroup {
     return this.fb.group({
       task: this.fb.control<string>('', [Validators.required]),
-      priority: this.fb.control<string>('', [Validators.required]),
-      start: this.fb.control<number>(new Date().getMilliseconds()),
-      due: this.fb.control(0, [Validators.required])
+      priority: this.fb.control<string>('Low', [Validators.required]),
+      start: this.fb.control<Date>(new Date()),
+      due: this.fb.control<Date>(new Date(), [Validators.required])
     })
   }
 
-  formDialog(){
-    this.visible=true
+  formDialog() {
+    this.visible = true
   }
 
-  addTask(){
+  addTask() {
     const task = this.taskForm.value as Task
-    this.tasks.push(task)
+    task.start = this.taskForm.value.start.getTime()
+    task.due = this.taskForm.value.due.getTime()
+
+    console.info(task.start)
+    task.completed = false
+    this.taskSvc.addTasksToWorkspace(this.uid, this.currentWorkspace, task)
   }
 }
