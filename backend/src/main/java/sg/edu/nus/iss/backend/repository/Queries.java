5264716f1730e22d6e8f94ff7807db933679d313
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-22 17:39:03 +0800

    queries for telegram bot table

--- backend/src/main/java/sg/edu/nus/iss/backend/repository/Queries.java
+++ backend/src/main/java/sg/edu/nus/iss/backend/repository/Queries.java
@@ -11,5 +11,24 @@ public class Queries {
         select * from user_details 
         where email=?
         """;
+
+    public final static String SQL_FIND_USER_BY_EMAIL_AND_ID = """
+        select * from user_details 
+        where email=? and id=?
+        """;
     
+    public final static String SQL_ADD_TELEGRAM_ACCOUNT = """
+        insert into telegram_bot(chatid, username, email, id)
+        values (?, ?, ?, ?)
+        """;
+
+    public final static String SQL_FIND_USER_BY_CHATID = """
+        select * from telegram_bot
+        where chatid=?
+        """;
+
+    public final static String SQL_GET_USERID_BY_CHATID = """
+        select id from telegram_bot
+        where chatid=?
+        """;
 }
