1c8bed3ec8c6291f1201c1fe3a9602ffa470085f
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-22 17:38:26 +0800

    dynamic keyboard data

--- backend/src/main/java/sg/edu/nus/iss/backend/telegram/KeyboardFactory.java
+++ backend/src/main/java/sg/edu/nus/iss/backend/telegram/KeyboardFactory.java
@@ -1,10 +1,15 @@
 package sg.edu.nus.iss.backend.telegram;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import sg.edu.nus.iss.backend.model.Task;
 
 public class KeyboardFactory {
     public static ReplyKeyboard linkAccount() {
@@ -13,27 +18,37 @@ public class KeyboardFactory {
         return new ReplyKeyboardMarkup(List.of(row));
     }
 
-    public static ReplyKeyboard getWorkspaces() {
-        KeyboardRow row1 = new KeyboardRow();
-        row1.add("workspace 1");
-        KeyboardRow row2 = new KeyboardRow();
-        row2.add("workspace 2");
-        KeyboardRow row3 = new KeyboardRow();
-        row3.add("workspace 3");
-        KeyboardRow row4 = new KeyboardRow();
-        row4.add("workspace 4");
-        return ReplyKeyboardMarkup.builder().keyboardRow(row1).keyboardRow(row2).keyboardRow(row3).keyboardRow(row4).build();
+    public static ReplyKeyboard getWorkspaces(List<String> workspaces) {
+        List<KeyboardRow> keyboardRows = new LinkedList<>();
+        for (String w : workspaces) {
+            KeyboardRow row = new KeyboardRow();
+            row.add(w);
+            keyboardRows.add(row);
+        }
+        // KeyboardRow row1 = new KeyboardRow();
+        // row1.add("workspace 1");
+        // KeyboardRow row2 = new KeyboardRow();
+        // row2.add("workspace 2");
+        // KeyboardRow row3 = new KeyboardRow();
+        // row3.add("workspace 3");
+        // KeyboardRow row4 = new KeyboardRow();
+        // row4.add("workspace 4");
+        return ReplyKeyboardMarkup.builder().keyboard(keyboardRows).build();
     }
 
-    public static ReplyKeyboard getTasks() {
-        KeyboardRow row1 = new KeyboardRow();
-        row1.add("task 1");
-        KeyboardRow row2 = new KeyboardRow();
-        row2.add("task 2");
-        KeyboardRow row3 = new KeyboardRow();
-        row3.add("task 3");
-        KeyboardRow row4 = new KeyboardRow();
-        row4.add("task 4");
-        return ReplyKeyboardMarkup.builder().keyboardRow(row1).keyboardRow(row2).keyboardRow(row3).keyboardRow(row4).build();
+    public static ReplyKeyboard getTasks(List<Task> tasks) {
+        List<KeyboardRow> keyboardRows = new LinkedList<>();
+        for (Task t : tasks) {
+            KeyboardRow row = new KeyboardRow();
+            row.add(t.getTask());
+            keyboardRows.add(row);
+        }
+        return ReplyKeyboardMarkup.builder().keyboard(keyboardRows).build();
+    }
+
+    public static ReplyKeyboard taskActions(){
+        InlineKeyboardButton edit = InlineKeyboardButton.builder().text("edit").callbackData("edit").build();
+        InlineKeyboardButton complete = InlineKeyboardButton.builder().text("mark as complete").callbackData("markcomplete").build();
+        return InlineKeyboardMarkup.builder().keyboardRow(List.of(edit)).keyboardRow(List.of(complete)).build();
     }
 }
