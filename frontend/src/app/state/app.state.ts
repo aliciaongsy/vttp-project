0fddef612cdaa650dbc6621e3015f0c8a2c5b86d
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-03-26 22:57:28 +0800

    add new planner state

--- frontend/src/app/state/app.state.ts
+++ frontend/src/app/state/app.state.ts
@@ -1,7 +1,9 @@
+import { PlannerState } from "./planner/planner.reducer";
 import { TaskState } from "./tasks/task.reducer";
 import { UserState } from "./user/user.reducer";
 
 export interface AppState{
     user: UserState
     task: TaskState
+    planner: PlannerState
 }
