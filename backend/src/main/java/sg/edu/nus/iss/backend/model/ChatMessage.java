818f48689d008a9a90c37f1a77e1f16a7cfc52c6
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-04-01 00:26:32 +0800

    updates

--- backend/src/main/java/sg/edu/nus/iss/backend/model/ChatMessage.java
+++ backend/src/main/java/sg/edu/nus/iss/backend/model/ChatMessage.java
@@ -1,5 +1,11 @@
 package sg.edu.nus.iss.backend.model;
 
+import org.bson.Document;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+
 public class ChatMessage {
 
     private String content;
@@ -27,6 +33,33 @@ public class ChatMessage {
     }
     public ChatMessage(){
         
+    }
+
+    public Document toDoc(ChatMessage message){
+        Document doc = new Document();
+        doc.put("content", message.getContent());
+        doc.put("sender", message.getSender());
+        doc.put("type", message.getType());
+        doc.put("timestamp", message.getTimestamp());
+        return doc;
+    }
+
+    public ChatMessage docToChatMessage(Document doc){
+        ChatMessage message = new ChatMessage();
+        message.setContent(doc.getString("content"));
+        message.setSender(doc.getString("sender"));
+        message.setType(MessageType.valueOf(doc.getString("type")));
+        message.setTimestamp(doc.getLong("timestamp"));
+        return message;
+    }
+
+    public JsonObject toJson(ChatMessage message){
+        JsonObjectBuilder b = Json.createObjectBuilder();
+        return b.add("content", message.getContent())
+            .add("sender", message.getSender())
+            .add("type", message.getType().toString())
+            .add("timestamp", message.getTimestamp())
+            .build();
     }
     
 }
