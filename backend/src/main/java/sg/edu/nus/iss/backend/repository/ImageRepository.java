799826361de7c5d1f78fd6ecede6f7f0b91c94d0
aliciaongsy <ONGS0135@e.ntu.edu.sg>
2024-04-06 16:18:08 +0800

    amazon s3

---
+++ backend/src/main/java/sg/edu/nus/iss/backend/repository/ImageRepository.java
@@ -0,0 +1,36 @@
+package sg.edu.nus.iss.backend.repository;
+
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Repository
+public class ImageRepository {
+    
+    @Value("${s3.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3;
+
+    public String saveImageTo3T(String id, ObjectMetadata metadata, InputStream is) throws SdkClientException, AmazonServiceException{
+
+        String key = "newsimages/%s".formatted(id);
+
+        PutObjectRequest putReq = new PutObjectRequest(bucketName, key, is, metadata);
+        putReq = putReq.withCannedAcl(CannedAccessControlList.PublicRead);
+
+        s3.putObject(putReq);
+
+        return s3.getUrl(bucketName, key).toExternalForm();
+    }
+}
